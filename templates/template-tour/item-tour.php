8141d23c9fe5330b66eb3e26176dceac6dd6dfc1
DangPhuongTay <89719935+DangPhuongTay@users.noreply.github.com>
2024-04-02 14:25:39 +0700

    update item tour

--- templates/template-tour/item-tour.php
+++ templates/template-tour/item-tour.php
@@ -1,64 +1,70 @@
 <?php global $product; ?>
 <div class="firstProduct__content--list__item">
-    <div class="firstProduct__content--list__item--wrap">
-        <a href="<?php the_permalink() ?>" class="">
-            <div class="firstProduct__img">
-                <?php echo get_the_post_thumbnail(get_the_ID(), 'thumnail', array( 'class' =>'thumnail') ); ?>  
-            </div>
-            <div class="firstProduct__info">
-                <div class="firstProduct__info--top">
-                    <div class="firstProduct__info--top__title text__overflow--main">
-                        <p>
-                            <?php
-
-                            the_title();
-
-                            ?>
-                        </p>
-                    </div>
-                    <div class="firstProduct__info--top__score" style="gap:7px;font-size: 11px;">
-                        <?php 
-                                foreach ($product->attributes as $taxonomy => $attribute) {
-                                    foreach ($attribute->get_terms() as $term) {
-                                        ?>
-                                         <span style="background: #eee;border-radius: 10px;padding: 6px 10px;">
-                                        <?php
-                                        echo  $term->name; 
-
-                                        ?>
-                                         </span>
-                                        <?php
-                                    }
-                                 }
-                                ?>
-                       
-                    </div>
-                    <!-- tag -->
-                    <div class="tagging__wrap">
-                        <div class="tagging__box">
-                            <div class="tagging__tag">
-                                <span class="tagging__tag--text">
-                                    <?php the_author_posts_link(); ?>
-                                </span>
-                            </div>
+                        <div class="firstProduct__content--list__item--wrap">
+                            <a href="../page/detail_tour.html" class="">
+                                <div class="firstProduct__img">
+                                    <img src="https://res.klook.com/image/upload/c_fill,w_550,h_308/fl_lossy.progressive,q_85/q_85/activities/y9w45f2fdciamxyq5evf.webp" alt="tourProduct">
+                                </div>
+                                <div class="firstProduct__info">
+                                    <div class="firstProduct__info--top">
+                                        <div class="firstProduct__info--top__title text__overflow--main">
+                                            <a href="#">Trải Nghiệm Canyoning
+                                                Vượt Thác Datanla</a>
+                                        </div>
+                                        <div class="firstProduct__info--top__score">
+                                            <div class="firstProduct__info--top__score--star">
+                                                <div class="firstProduct__info--top__score--star__icon">
+                                                    <ion-icon name="star"></ion-icon>
+                                                </div>
+                                                <div class="firstProduct__info--top__score--star__number">
+                                                    <span>4.9</span>
+                                                </div>
+                                            </div>
+                                            <span class="firstProduct__info--top__score--review">(120)</span>
+                                            <!-- sep -->
+                                            <div class="sep">
+                                                <ion-icon name="ellipse"></ion-icon>
+                                            </div>
+                                            <!-- <div class="firstProduct__info--top__score--booked"> -->
+                                                <span firstProduct__info--top__score--booked__number>1K+
+                                                    Đã được đặt </span>
+                                            <!-- </div> -->
+                                        </div>
+                                        <!-- tag -->
+                                        <div class="tagging__wrap">
+                                            <div class="tagging__box">
+                                                <div class="tagging__tag">
+                                                    <span class="tagging__tag--text">Hướng
+                                                        dẫn tiếng Việt</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- bottom -->
+                                    <div class="firstProduct__info--bottom">
+                                        <div class="firstProduct__info--bottom__content">
+                                            <div class="firstProduct__info--bottom__content--box">
+                                                <div class="firstProduct__info--bottom__content--box__price">
+                                                    <span>đ 1,890,000</span>
+                                                </div>
+                                                <div class="firstProduct__info--bottom__content--box__underline">
+                                                    <span>đ 2,050,000</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- button policy -->
+                                        <div class="firstProduct__info--bottom__tagging tagging__wrap">
+                                            <div class="tagging__box firstProduct__info--bottom__tagging--box">
+                                                <div class="tagging__tag">
+                                                    <span
+                                                        class="tagging__tag--text firstProduct__info--bottom__tagging--box__text">Chính
+                                                        sách đảm bảo về
+                                                        giá</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
                     </div>
-                </div>
-                <!-- bottom -->
-                <div class="firstProduct__info--bottom">
-                    <div class="firstProduct__info--bottom__content">
-                        <div class="firstProduct__info--bottom__content--box">
-                            <div class="firstProduct__info--bottom__content--box__price">
-
-
-                                <?php echo $product->get_price_html(); ?>
-                            
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </a>
-    </div>
-</div>
