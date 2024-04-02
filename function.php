204bad1ca8c44b6d90106fc463d5c0823416fd50
TienDat <95626306+LMTTienDat@users.noreply.github.com>
2024-04-02 09:29:17 +0700

    push

--- function.php
+++ function.php
@@ -6,4 +6,13 @@
     add_theme_support('woocommerce');
     }
 
+    function mytheme_add_woocommerce_support() {
+        add_theme_support( 'woocommerce' );
+    }
+    
+    add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support');
+    function wpbootstrap_enqueue_styles() {
+        wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' );
+        }
+        add_action('wp_enqueue_scripts', 'wpbootstrap_enqueue_styles');
 ?>
