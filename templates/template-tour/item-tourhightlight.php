e89b65de15ae1349502877196ae2a7086d913932
PhuongThaoCTK44 <2011442@dlu.edu.vn>
2024-04-04 15:46:27 +0700

    update

--- templates/template-tour/item-tourhightlight.php
+++ templates/template-tour/item-tourhightlight.php
@@ -1,13 +1,22 @@
-
-<div class="hightlightProduct__content--list__item">
+<?php global $product; ?>
+    <div class="hightlightProduct__content--list d-flex align-items-center">
                                     <div class="hightlightProduct__content--list__item--wrap">
+<a href="<?php the_permalink(); ?>">
+                                        
                                         <div class="hightlightProduct__img">
                                             <img src="https://res.klook.com/image/upload/c_fill,w_550,h_308/fl_lossy.progressive,q_85/q_85/activities/rkfj4bj4rtumbodna0fn.webp">
                                         </div>
                                         <div class="hightlightProduct__title text__overflow--main">
-                                            <a href="#" class="hightlightProduct__link">Tour
-                                                Phiêu Lưu Đi Trek Núi Langbiang Ở Đà
-                                                Lạt</a>
+                                            <p class="hightlightProduct__link">
+                                                <?php
+
+                                                the_title();
+                                    
+                                                ?>
+                                            </p>
                                         </div>
+                                    </a>
                                     </div>
                                 </div>
+                           
+                           
