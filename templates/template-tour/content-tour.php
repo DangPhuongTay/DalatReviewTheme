4b850ddfe2fe643c3d010f82913cbfac3df8aa53
PhuongThaoCTK44 <2011442@dlu.edu.vn>
2024-04-04 11:02:44 +0700

    update

--- templates/template-tour/content-tour.php
+++ templates/template-tour/content-tour.php
@@ -5,7 +5,67 @@
             the_post();
             ?>
             <?php global $product; ?>
-                <section class="detail__tour">
+            <div class="slide__img--box">
+                <ion-icon class="slide__img--close" name="close-outline" onclick="closeSlides()"></ion-icon>
+                <div class="container__slides">
+                <?php  if ( ! function_exists( 'wc_get_gallery_image_html' ) ) {
+                    return;
+                }
+                
+                
+                $attachment_ids = $product->get_gallery_image_ids();
+                
+                if ( $attachment_ids && $product->get_image_id() ) {
+                    foreach ( $attachment_ids as $attachment_id ) {
+                        ?>
+                        <div class="mySlides" style="width: 800px;display: flex;align-items: center;justify-content: center;">
+                        <?php
+                            echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html( $attachment_id ), $attachment_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+                        ?>
+                        </div>
+                    <?php
+                    }
+                } ?>
+    
+    
+    
+                <!-- Next and previous buttons -->
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    
+                <!-- Image text -->
+                <div class="caption-container">
+                    <p id="caption"></p>
+                </div>
+    
+                <!-- Thumbnail images -->
+                <div class="container__slide--number">
+                <?php  if ( ! function_exists( 'wc_get_gallery_image_html' ) ) {
+                    return;
+                }
+                
+                
+                $attachment_ids = $product->get_gallery_image_ids();
+                
+                if ( $attachment_ids && $product->get_image_id() ) {
+                    foreach ( $attachment_ids as $attachment_id ) {
+                        ?>
+                       <div class="column">
+                        <?php
+                            echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html( $attachment_id ), $attachment_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+                        ?>
+                        </div>
+                    <?php
+                    }
+                } ?>
+    
+                </div>
+            </div>
+        </div>
+
+
+        
+    <section class="detail__tour">
         <div class="detail__tour--content__right--mobile shadow">
             <p><span>đ</span> <span class="detail__tour--content__right--price">245.000</span></p>
             <button class="detail__tour--content__right--btn firstBtn">
@@ -656,10 +716,11 @@ Khi tải ứng dụng Đà Lạt Review Tất Tần Tật bạn sẽ nhận đ�
         </div>
     </section>
     </section>
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/header.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/detail_tour.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/slide_img.js"></script>
             <?php
-            the_category();
-            the_title();
-            the_content();
+
         }
     }
 ?>
