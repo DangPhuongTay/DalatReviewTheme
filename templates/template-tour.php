7301591258fa6570dac8a99dec6dcc49c6fbcd73
DangPhuongTay <89719935+DangPhuongTay@users.noreply.github.com>
2024-04-05 10:50:02 +0700

    update theme

--- templates/template-tour.php
+++ templates/template-tour.php
@@ -26,6 +26,14 @@
                     </div>
                     <div class="tour__banner--search__input">
                         <input placeholder="Tìm kiếm" type="text">
+                       
+                        <form action="<?php echo home_url(); ?>/" id="search-form" method="get">
+                            <input type="text" name="s" id="s" value="type your search" onblur="if(this.value=='')this.value='type your search'"
+                            onfocus="if(this.value=='type your search')this.value=''" />
+                            <input type="hidden" value="submit" />
+                        </form>
+                            
+ 
                     </div>
                 </div>
             </div>
