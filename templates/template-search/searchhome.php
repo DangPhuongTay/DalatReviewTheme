798b3d83dad0d0dea5feb0f8dd923651e4e086d0
ThanhTinh <93694384+thanhtinhht2002@users.noreply.github.com>
2024-04-08 09:09:31 +0700

    fix

--- templates/template-search/searchhome.php
+++ templates/template-search/searchhome.php
@@ -1,8 +1,8 @@
-<div class="home__slider--serchbox__search">
+<div class="home__slider--searchbox__search">
     <ion-icon name="search"></ion-icon>
-<form style="width:100%;" action="<?php echo home_url(); ?>" id="search-form" method="get">
+<form class="home__slider--searchbox__element" style="width:100%;" action="<?php echo home_url(); ?>" id="search-form" method="get">
     <input type="hidden" name="post_type" value="product"> 
-    <input style="width:80%;" class="home__slider--serchbox__search--input" type="text" name="s" id="s" value="Tour săn mây" onblur="if(this.value=='')this.value='Tour săn mây'"
+    <input class="home__slider--searchbox__element--left" style="width:80%;" class="home__slider--searchbox__search--input" type="text" name="s" id="s" value="Tour săn mây" onblur="if(this.value=='')this.value='Tour săn mây'"
     onfocus="if(this.value=='Tour săn mây')this.value=''" />
     <button class="home__slider--searchbox__btn" type="submit" >Khám phá</button>
 </form>
