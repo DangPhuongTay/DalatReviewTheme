c8e76bb538451a874449793b1dd82764f0f23d08
NgocAnhsne <ngocanhne2402@gmail.com>
2024-04-08 15:44:36 +0700

    search

---
+++ templates/template-blog/blog-banner.php
@@ -0,0 +1,7 @@
+<div class="blog__banner">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/blog-banner.png" alt="">
+            <div class="blog__banner--text">
+                <p>Bí kíp trải nghiệm trọn vẹn Đà Lạt</p>
+                <span>Khám phá những câu chuyện, cảm hứng và ý tưởng du lịch Đà Lạt độc đáo cho chuyến vi vu tiếp theo của bạn</span>
+            </div>
+        </div>
---
+++ templates/template-home/home-firstTagMobile.php
@@ -0,0 +1,101 @@
+<div class="firstTag__internal--mobile">
+
+        <div class="firstTag__internal--wrap">
+            <h2 class="firstTag__internal--title">Hấp dẫn không kém</h2>
+
+            <!-- module-banner -->
+            <div class="firstTag__internal--content">
+                <h3 class="firstTag__internal--header">
+                    Khám phá các hoạt động phổ biến
+                </h3>
+                <ul class="firstTag__internal--wrapper">
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Warner Bros Studio Tour Tokyo</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Ba Na Hills</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Grand World Phú QuốC</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">xe Huế- Đà Nẵng</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Vinwonders Phú QuốC</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Saigon Skydeck</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Taipei 101 Ticket</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Garden By The Bay</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Vinpearl Safari Phú QuốC</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Xe Limousine Đi Vũng Tàu</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Puppy Farm</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Vé Cáp Treo Núi Bà Đen</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Xe Limousine Hà NộI Hạ Long</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Universal Studios Japan</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Artscience Museum</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Tropicana Park</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Sun World Hạ Long</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">London Eye Ticket</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Limousine Hà NộI Ninh Bình</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Universal Singapore</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">BảO Tàng Thế GiớI Cà Phê</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Sagrada Familia</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Bts Skytrain Bangkok</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Ký ỨC HộI An</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Vinwonders Nam HộI An</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Cung ĐiệN Versailles</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Fresh Garden Đà LạT</a>
+                    </li>
+                    <li class="firstTag__internal--unit">
+                        <a href="" class="firstTag__unit--suggestion">Landmark 81 Skyview</a>
+                    </li>
+
+
+                </ul>
+            </div>
+        </div>
+    </div>
