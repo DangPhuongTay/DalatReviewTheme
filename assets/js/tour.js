fb7c2696aa51ac0e836ba5d791f86ba8d1617709
NgocAnhsne <ngocanhne2402@gmail.com>
2024-04-09 11:23:36 +0700

    comment tour

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -14,19 +14,7 @@ $(".firstQuestion__content--list__item").click(function(e) {
   e.stopPropagation();
 });
 
-// window.primaryDropdown = function() {
-//     document.getElementById("primaryDropdownHidden").classList.toggle("show");
-//   };
-
-// window.onclick = function(event) {
-//   if (!event.target.matches('.primary__dropdown--button')) {
-//     var dropdowns = document.getElementsByClassName("primary__dropdown--content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// };
+function btnReviewTour(){
+  tourBody.classList.add('listTourReview')
+  tourBody.classList.add('tourBlur')  
+}
--- templates/template-tour/tour-firstReview.php
+++ templates/template-tour/tour-firstReview.php
@@ -1,35 +1,47 @@
+<?php global $product; ?>
+<?php global $rating_html; ?>
+
 <div class="firstReview">
-                <h2 class="tour__Title">
-                    Mọi người nói gì về Tour & Trải nghiệm ở Đà Lạt
-                </h2>
-                <div class="firstReview__card container-fluid">
-                    <div class="firstReview__card">
-                        <div class="firstReview__card--item ">
-                            <div class="scoreStar">
-                                <ion-icon name="star"></ion-icon>
-                                <ion-icon name="star"></ion-icon>
-                                <ion-icon name="star"></ion-icon>
-                                <ion-icon name="star"></ion-icon>
-                                <ion-icon name="star"></ion-icon>
-                            </div>
-                            <div class="firstReview__for">
-                                <span>Đánh giá cho: </span>
-                                <a href="/detail_tour">Đi Xe Trượt Thác Datanla New Alpine
-                                    Tại
-                                    Đà Lạt</a>
-                            </div>
-                            <div class="firstReview__time">
-                                <span>Người dùng</span>
-                                Đăng 229 ngày trước
-                            </div>
-                            <div class="firstReview__content">
-                                Đặt hàng qua Đà Lạt Review tiết kiệm được chi
-                                phí vì có chương trình giảm giá. mọi người nhớ
-                                đổi ra vé giấy rồi hãy vào xếp hàng để lên xe
-                                trượt nhé.
-                            </div>
-                        </div>
-                        <div class="firstReview__card--item ">
+    <div class="firstReview__header">
+        <h2 class="tour__Title">
+            Mọi người nói gì về Tour & Trải nghiệm ở Đà Lạt
+        </h2>
+    </div>
+    <div class="firstReview__card">
+        <div class="firstReview__card">
+            <!-- <div class="firstReview__card--item ">
+                <div class="scoreStar">
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                    <ion-icon name="star"></ion-icon>
+                </div>
+                <div class="firstReview__for">
+                    <span>Đánh giá cho: </span>
+                    <a href="/detail_tour">Đi Xe Trượt Thác Datanla New Alpine
+                        Tại
+                        Đà Lạt</a>
+                </div>
+                <div class="firstReview__time">
+                    <span>Người dùng</span>
+                    Đăng 229 ngày trước
+                </div>
+                <div class="firstReview__content">
+                    Đặt hàng qua Đà Lạt Review tiết kiệm được chi
+                    phí vì có chương trình giảm giá. mọi người nhớ
+                    đổi ra vé giấy rồi hãy vào xếp hàng để lên xe
+                    trượt nhé.
+                </div> -->
+                <div class="firstReview__card--item">
+                    <?php
+                    $args = array('post_type' => $product);
+                    $comments = get_comments($args);
+                    wp_list_comments(array('callback' => 'woocommerce_comments'), $comments);
+                    ?>
+                </div>
+        </div>
+        <!-- <div class="firstReview__card--item ">
                             <div class="scoreStar">
                                 <ion-icon name="star"></ion-icon>
                                 <ion-icon name="star"></ion-icon>
@@ -50,12 +62,12 @@
                                 Đường trượt mới rất đã, cảm giác an toàn hơn.
                                 Trải nghiệm high rope cũng rất tuyệt.
                             </div>
-                        </div>
-                    </div>
-                    <!-- nextBtn -->
-                    <!-- <div class="nextBtn">
+                        </div> -->
+    </div>
+    <!-- nextBtn -->
+    <!-- <div class="nextBtn">
                             <ion-icon name="chevron-forward-outline"></ion-icon>
                         </div> -->
-                </div>
-    
-            </div>
+</div>
+
+</div>
