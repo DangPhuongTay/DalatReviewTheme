9284daba936285b26efacf40631c6ba11ad835c1
ThanhTinh <93694384+thanhtinhht2002@users.noreply.github.com>
2024-04-11 12:53:32 +0700

    put

--- templates/template-slide/slide-image.php
+++ templates/template-slide/slide-image.php
@@ -6,8 +6,6 @@
                         return;
                     }
              
-
-
                     $attachment_ids = $product->get_gallery_image_ids();
                     if ($attachment_ids && $product->get_image_id()) {
                         foreach ($attachment_ids as $attachment_id) {
@@ -23,7 +21,7 @@
                     } ?>
 
                 </div>   
-                    
+
                     <!-- Next and previous buttons -->
                     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
                     <a class="next" onclick="plusSlides(1)">&#10095;</a>
