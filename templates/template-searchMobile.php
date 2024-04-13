69227c1a8de1ce8e4266bfe391c29ec962a85824
TienDat <95626306+LMTTienDat@users.noreply.github.com>
2024-04-13 11:37:24 +0700

    update search home

---
+++ templates/template-searchMobile.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: TemplateSearchMobile
+ * Display Only Search Mobile template
+ * 
+ * @package WordPress
+ * @subpackage eminent
+ * @since eminent 1.0
+ * 
+ */
+get_header(); ?>
+
+<section class="searchMoblie">
+<?php wc_get_template('templates/template-search/searchliveshow.php'); ?>
+</section>
