52f1fe3aefb0b8515406db4c05849aa2e3e13eac
NgocAnhsne <ngocanhne2402@gmail.com>
2024-04-23 15:23:17 +0700

    123

--- templates/template-tour/item-tourhightlight.php
+++ templates/template-tour/item-tourhightlight.php
@@ -1,24 +1,20 @@
-
 <?php global $product; ?>
-    <div class="hightlightProduct__content--list d-flex align-items-center">
-                                    <div class="hightlightProduct__content--list__item--wrap">
-                                            <a href="<?php the_permalink(); ?>">
-                                        
-                                        <div class="hightlightProduct__img">
-                                            <?php echo get_the_post_thumbnail(get_the_ID(), 'thumnail', array( 'class' =>'thumnail') ); ?>
-                                        </div>
-                                        <div class="hightlightProduct__title text__overflow--main">
-                                            <p class="hightlightProduct__link">
-                                                <?php
+<div class="hightlightProduct__content--list d-flex align-items-center">
+    <div class="hightlightProduct__content--list__item--wrap">
+        <a href="<?php the_permalink(); ?>">
 
-                                                the_title();
-                                    
-                                                ?>
-                                            </p>
-                                        </div>
-                                    </a>
-                                    </div>
-                                </div>
-                           
-                           
+            <div class="hightlightProduct__img">
+                <?php echo get_the_post_thumbnail(get_the_ID(), 'thumnail', array('class' => 'thumnail')); ?>
+            </div>
+            <div class="hightlightProduct__title text__overflow--main">
+                <p class="hightlightProduct__link">
+                    <?php
 
+                    the_title();
+
+                    ?>
+                </p>
+            </div>
+        </a>
+    </div>
+</div>
