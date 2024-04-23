dd3c4c154b4cc461b66b581b9fc0058ffd8e8da1
DangPhuongTay <89719935+DangPhuongTay@users.noreply.github.com>
2024-04-23 15:21:00 +0700

    update theme

--- templates/template-liveshow/item-detailliveshow.php
+++ templates/template-liveshow/item-detailliveshow.php
@@ -4,16 +4,7 @@
   <div class="popup__item">
     <div class="popup__item--name">
     <?php
-            function the_title_trim($title)
-            {
-            $pattern[0] = '/Protected:/';
-            $pattern[1] = '/Private:/';
-            $replacement[0] = ''; // Enter some text to put in place of Protected:
-            $replacement[1] = ''; // Enter some text to put in place of Private:
-            return preg_replace($pattern, $replacement, $title);
-            }
-            add_filter('the_title', 'the_title_trim');
-          the_title();
+        substr(the_title(), 6);
        ?>
     </div>
     <div class="popup__item--price">
