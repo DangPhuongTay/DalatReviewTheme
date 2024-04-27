c269c43c986061b442716687dda078d758e430e4
ThanhTinh <93694384+thanhtinhht2002@users.noreply.github.com>
2024-04-27 13:17:57 +0700

    push update

--- templates/template-home/item-voucher.php
+++ templates/template-home/item-voucher.php
@@ -1,5 +1,5 @@
+
 <div class="home__sale--banner__container--list__item">
-   
                         <?php the_content(); ?>
         <!-- <a href="  <?php the_permalink(); ?>"> -->
         <!-- <img src=" <?php echo get_the_post_thumbnail_url(get_the_ID(), 'thumbnail'); ?>" alt=""> -->
@@ -7,7 +7,6 @@
                         <div class="home__sale--banner__container--list__item--text">
                         <?php the_title(); ?>
                         </div>
-
                 </div>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/voucher.js"></script>
 
