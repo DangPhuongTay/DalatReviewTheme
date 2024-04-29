5f8e9001775b41094d1aa0aa2727140583ad19c1
DangPhuongTay <89719935+DangPhuongTay@users.noreply.github.com>
2024-04-29 16:40:28 +0700

    update theme

--- templates/template-liveshow/content-liveshow.php
+++ templates/template-liveshow/content-liveshow.php
@@ -1,3 +1,7 @@
-<?php
-    wc_get_template_part('single-product' ); 
-?>
+
+<?php while ( have_posts() ) : ?>
+	<?php the_post(); ?>
+
+	<?php wc_get_template_part( 'content', 'single-product' );?>
+
+<?php endwhile; // end of the loop. ?>
