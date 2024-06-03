42f7e796af80dfd1016b305bd2630d7cd52a9e39
ThanhTinh <thanhtinhht2002@gmail.com>
2024-06-03 09:57:35 +0700

    uopdate blog
    132

--- templates/template-blog/blog-news-header.php
+++ templates/template-blog/blog-news-header.php
@@ -1,6 +1,15 @@
 <div class="blog__container--new__header">
+   <div class="blog__container--new__header--tags">
+   <div>
+        Chủ đề: Ẩm thực Đà Lạt
+    </div>
+    <div>
+        Số bài viết: 74
+    </div>
+   </div>
     <p>Bài viết mới nhất</p>
 </div>
+
 <div class="blog__container--new__list">
     <?php  get_template_part('templates/template-blog/archive', 'blog') ?>  
 </div>
